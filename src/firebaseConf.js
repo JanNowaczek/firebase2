8c229713916adbb074490ab1f70bc944906fa316
mrozo2m <mrozo2m@interia.pl>
2019-04-10 12:06:44 +0200

    export firebaseConf added

--- src/firebaseConf.js
+++ src/firebaseConf.js
@@ -1,12 +1,14 @@
 import firebase from 'firebase'
 
-consts config = {
+const config = {
     apiKey: 'AIzaSyDDOwPtv46O8gP0f-RBsvT9nDp3yrRXxtQ',
     authDomain: 'ad-snadbox.firebaseapp.com',
     databaseURL: 'https://ad-snadbox.firebaseio.com',
     projectId: 'ad-snadbox',
     storageBucket: 'ad-snadbox.appspot.com',
     messagingSenderId: '328854615659'
-  }
-  
-  firebase.initializeApp(config)
+}
+
+firebase.initializeApp(config)
+
+export const database = firebase.database()
