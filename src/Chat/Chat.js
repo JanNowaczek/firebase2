6b80571de94f9f7b299385b1b4441d956f21ce9d
mrozo2m <mrozo2m@interia.pl>
2019-04-10 13:32:22 +0200

    syncing masseges in component DidMount

--- src/Chat/Chat.js
+++ src/Chat/Chat.js
@@ -1,6 +1,24 @@
 import React from 'react'
 
+import { database } from '../firebaseConf'
+
 class Chat extends React.Component{
+    state = {
+        messages: null,
+    }
+
+    componentDidMount(){
+        database.ref('/JFDDL7/messages')
+        .on(
+            'value',
+            (snapshot) => {
+                this.setState({
+                    messages: snapshot.val(),
+                })
+            } 
+        )
+    }
+
     render(){
         return(
             <div>
