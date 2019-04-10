2e2ccb0d98095f26d0e80bcd7a9143907169a144
mrozo2m <mrozo2m@interia.pl>
2019-04-10 12:17:27 +0200

    read data once, no display

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React from 'react'
 
+import { database } from './firebaseConf'
+
+const courseRef = database.ref('JFDDL7')
+
+courseRef.once(
+    'value'
+)
+    .then(snapshot => console.log(snapshot.val()))
+
 const App = (props) => (
     <div>
         App
