b7ae84eeab6a80c18a423c84b42eaaba3f753710
mrozo2m <mrozo2m@interia.pl>
2019-04-10 13:23:27 +0200

    Chat added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react'
 
-import { database } from './firebaseConf'
 
-const result = database.ref('/JFDDL7').on(
-    'value',
-    snapshot => console.log(snapshot.val())
-)
-
-console.log(result)
 
 const App = (props) => (
     <div>
         App
+        
     </div>
 )
 
